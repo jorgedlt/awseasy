d3234a9242107b8f55e0f5fc7954063d1a851ff2
Jorge de la Torre <jorgedlt@gmail.com>
2017-03-28 10:53:35 -0400

    Commit message

---
+++ install.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+# Install.sh - for awseasy
+
+# my awseasy
+echo; echo "${GREEN}" ~/.awseasy/awseasy "${YELLOW}"
+rsync -av --progress "$HOME/code/awseasy" "$HOME/.awseasy/" --exclude ".git"
+
+echo; echo "${GREEN}"REPO Files "${YELLOW}"
+ls -la  "$HOME/code/awseasy/"
+
+echo; echo "${GREEN}" In Situ Files "${YELLOW}"
+ls -la  "$HOME/.awseasy/"
+
+echo "${RESET}"
+
+# .bashrc
+ [ -f "$HOME/.bashrc" ] && source "$HOME/.bashrc" || :
+
+# Basic Functions
+ [ -f "$HOME/.bash-func.sh" ] && source "$HOME/.bash-func.sh" || :
+
+# AWS Credentials
+ [ -f "$HOME/.awseasy/awseasy" ] && source "$HOME/.awseasy/awseasy" || :
+ [ -f "$HOME/.aws-keys" ] && source "$HOME/.aws-keys" || :
+
+exit
